6e85a093669c74c8382d794ebc921756dbafab04
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-12-25 16:57:23 +0500

    delete get job

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -113,8 +113,29 @@ export const getAllJobs = catchAsyncErrors(async (req, res) => {
   });
 });
 
-export const getMyJobs = catchAsyncErrors(async (req, res) => {});
+export const getMyJobs = catchAsyncErrors(async (req, res) => {
 
-export const deleteJob = catchAsyncErrors(async (req, res) => {});
+    const myJobs = await Job.find({ postedBy: req.user.id });
+    res.status(200).json({
+      success: true,
+      myJobs,
+      count: myJobs.length,
+    });
+
+});
+
+export const deleteJob = catchAsyncErrors(async (req, res) => {
+
+    const {id} = req.params;
+    const job = await Job.findById(id);
+    if(!job){
+        return next(new ErrorHandler("Job not found", 404));
+    }
+    await job.deleteOne();
+    res.status(200).json({
+      success: true,
+      message: "Job deleted successfully",
+    });
+});
 
 export const getASingleJob = catchAsyncErrors(async (req, res) => {});
