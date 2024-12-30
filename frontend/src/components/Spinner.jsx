89306079bc26cdaa731f134c13017e4983e739e2
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-12-30 16:09:34 +0500

    Update Spinner.jsx

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,8 +1,21 @@
+import React from "react";
+import { ClipLoader } from "react-spinners";
 
 const Spinner = () => {
   return (
-    <div>Spinner</div>
-  )
-}
+    <>
+      <section
+        style={{
+          minHeight: "525px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ClipLoader size={150} />
+      </section>
+    </>
+  );
+};
 
-export default Spinner
+export default Spinner;
