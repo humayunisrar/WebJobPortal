05202199f1be773fa31556e4a3760337fda12403
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-12-30 15:15:11 +0500

    Nav & Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,89 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  FaSquareXTwitter,
+  FaSquareInstagram,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa6";
 
 const Footer = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer>
+        <div>
+          <img src="/logo.png" alt="logo" />
+        </div>
+        <div>
+          <h4>Support</h4>
+          <ul>
+            <li>Sir Syed University Lumber 1 University of Pakistan</li>
+            <li>humayunisrar31@gmail.com</li>
+            <li>+92 333123453</li>
+          </ul>
+        </div>
 
-export default Footer
+        <div>
+          <h4>Quick Links</h4>
+          <ul>
+            <li to={"/"}>
+              <Link>Home</Link>
+            </li>
+            <li to={"/jobs"}>
+              <Link>Jobs</Link>
+            </li>
+            {isAuthenticated && (
+              <li>
+                <Link to={"/dashboard"}>Dashboard</Link>
+              </li>
+            )}
+          </ul>
+        </div>
+        <div>
+          <h4>Follow Us</h4>
+          <ul>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaSquareXTwitter />
+                </span>
+                <span>Twitter (X)</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaSquareInstagram />
+                </span>
+                <span>Instagram</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaYoutube />
+                </span>
+                <span>Youtube</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaLinkedin />
+                </span>
+                <span>LinkedIn</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <div className="copyright">
+        &copy; CopyRight 2024. All Rights Reserved.
+      </div>
+    </>
+  );
+};
+
+export default Footer;
