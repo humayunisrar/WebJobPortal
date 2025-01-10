c86b23785fd8fd1ab26a5ee0133c30f8c0e8c982
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2025-01-11 02:02:41 +0500

    .

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,23 +1,21 @@
 export const sendToken = (user, statusCode, res, message) => {
+
     // Create JWT token
     const token = user.getJwtToken();
 
-    // Cookie options
+    //
     const options = {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true, // Ensures the cookie is not accessible via JavaScript
-        secure: process.env.NODE_ENV === 'production', // Only set the secure flag in production
-        sameSite: 'strict' // Strict SameSite option for cross-site cookie security
+        httpOnly: true
+        secure: process.env.NODE_ENV === 'production' ? true : false
     };
 
-    // Send token in cookie and in response
-    res.status(statusCode)
-       .cookie('token', token, options)
-       .json({
-           success: true,
-           token, // Token in body is optional but helpful for immediate use
-           message
-       });
-};
+    res.status(statusCode).cookie('token', token, options).json({
+        success: true,
+        token,
+        message
+    });
+
+}
