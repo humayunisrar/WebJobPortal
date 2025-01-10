4da2325ddf520ef3ff8f258302d800292ba6f27c
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2025-01-11 02:04:49 +0500

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -8,7 +8,7 @@ export const sendToken = (user, statusCode, res, message) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true
+        httpOnly: true,
         secure: process.env.NODE_ENV === 'production' ? true : false
     };
 
